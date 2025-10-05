496ac83e23c200554bfafcbdd5423d4ffe0f885e
Caio Micael <contatocaiomicael@gmail.com>
2025-10-05 00:35:48 -0300

    style: ident code

--- api/Infrastructure/Data/AppDbContext.cs
+++ api/Infrastructure/Data/AppDbContext.cs
@@ -5,9 +5,7 @@ namespace bennerGrife.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
 
         public DbSet<CategoryModel> Category { get; set; }
 
